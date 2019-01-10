b1f1e421fdd501c57c8db4ae67ae87b0c7f4f260
rickrizzle <rickh39@gmail.com>
2019-01-10 15:30:58 -0500

    modified again

--- variables.rb
+++ variables.rb
@@ -11,3 +11,19 @@ puts composer + ' was "da bomb", in his day.'
 
 composer = 'Beethoven'
 puts 'But I prefer ' + composer +  ', personally.'
+
+var = 'just anoter ' + 'string'
+puts var
+var = 5 * (1+2)
+puts var
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
