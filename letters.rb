4cebd0c0a2c8de3c3f91ccf54a7a9ac69299ceff
rickrizzle <rickh39@gmail.com>
2019-01-10 15:10:01 -0500

    initial commit

---
+++ letters.rb
@@ -0,0 +1,29 @@
+puts 'Hello world!'
+puts ''
+puts 'Good-bye.'
+
+puts 'I like' + 'apple pie.' #no space; incorrect grammar when output
+puts 'I like ' + 'apple pie.' #correct grammar!
+puts 'blink ' * 4
+
+# Difference between numbers and digits:
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+## will produce error
+#puts '12' + 12
+#puts '2' * '5'
+
+# (will produce error) puts 'You're swell!'
+puts 'You\'re swell!'
+
+#More examples of escape character usage for backslash
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
