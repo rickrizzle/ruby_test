db0f08a870a15e3db046705fc2c643dea58ab247
rickrizzle <rickh39@gmail.com>
2019-01-10 15:27:30 -0500

    modfified commit

---
+++ variables.rb
@@ -0,0 +1,13 @@
+myString = '...you can say that again...'
+puts myString
+puts myString
+
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name + ' is a really long name!'
+
+composer = 'Mozart'
+puts composer + ' was "da bomb", in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer +  ', personally.'
